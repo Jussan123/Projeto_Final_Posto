b3f2a6f3ed7ec2b7fae572a7eb48355cdc1dbea1
erichformiga <erichformiga@gmail.com>
2023-05-29 19:53:10 -0300

    View

--- Program.cs
+++ Program.cs
@@ -1,16 +1,112 @@
-namespace PostoCombustivel;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ListBomba;
 
-static class Program
+
+namespace View
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    public partial class ProgramForm : Form
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
-    }    
+        private ToolStripStatusLabel dateTimeLabel; //Adicionado o rótulo para exibir a data e a hora
+
+        public ProgramForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            //Configurações do menu de navegação
+            MenuStrip menuStrip = new MenuStrip();
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Size = new Size(300, 24);
+
+            ToolStripMenuItem sistemaMenuItem = new ToolStripMenuItem("Sistema");
+
+            //Sub-menus do menu "Sistema"
+            ToolStripMenuItem produtosMenuItem = new ToolStripMenuItem("Login"); //Renomeado para "Login"
+            produtosMenuItem.Click += new EventHandler(ProdutoMenuItem_Click);
+            sistemaMenuItem.DropDownItems.Add(produtosMenuItem);
+
+            ToolStripMenuItem alxMenuItem = new ToolStripMenuItem("Funcionários"); //Renomeado para "Funcionários"
+            alxMenuItem.Click += new EventHandler(AlxMenuItem_Click);
+            sistemaMenuItem.DropDownItems.Add(alxMenuItem);
+
+            sistemaMenuItem.DropDownItems.Add(new ToolStripSeparator()); //Separador
+
+            sistemaMenuItem.DropDownItems.Add("Sair", null, SairMenuItem_Click);
+
+            menuStrip.Items.Add(sistemaMenuItem);
+
+            ToolStripMenuItem cadastroMenuItem = new ToolStripMenuItem("Cadastro");
+
+            //Sub-menus do menu "Cadastro"
+            ToolStripMenuItem bobasMenuItem = new ToolStripMenuItem("Bobas");
+            bobasMenuItem.Click += new EventHandler(BobasMenuItem_Click);
+            cadastroMenuItem.DropDownItems.Add(bobasMenuItem);
+
+            ToolStripMenuItem combustiveisMenuItem = new ToolStripMenuItem("Combustíveis");
+            combustiveisMenuItem.Click += new EventHandler(CombustiveisMenuItem_Click);
+            cadastroMenuItem.DropDownItems.Add(combustiveisMenuItem);
+
+            ToolStripMenuItem tipoCombustiveisMenuItem = new ToolStripMenuItem("Tipo Combustíveis");
+            tipoCombustiveisMenuItem.Click += new EventHandler(TipoCombustiveisMenuItem_Click);
+            cadastroMenuItem.DropDownItems.Add(tipoCombustiveisMenuItem);
+
+            ToolStripMenuItem fornecedorMenuItem = new ToolStripMenuItem("Fornecedor");
+            fornecedorMenuItem.Click += new EventHandler(FornecedorMenuItem_Click);
+            cadastroMenuItem.DropDownItems.Add(fornecedorMenuItem);
+
+            cadastroMenuItem.DropDownItems.Add(new ToolStripSeparator()); // Separador
+
+            cadastroMenuItem.DropDownItems.Add("Sair", null, SairMenuItem_Click);
+
+            menuStrip.Items.Add(cadastroMenuItem);
+
+            ToolStripMenuItem movimentacoesMenuItem = new ToolStripMenuItem("Movimentações");
+
+            //Sub-menus do menu "Movimentações"
+            ToolStripMenuItem fluxosMenuItem = new ToolStripMenuItem("Fluxos");
+            fluxosMenuItem.Click += new EventHandler(FluxosMenuItem_Click);
+            movimentacoesMenuItem.DropDownItems.Add(fluxosMenuItem);
+
+            menuStrip.Items.Add(movimentacoesMenuItem);
+
+            ToolStripMenuItem ajudaMenuItem = new ToolStripMenuItem("Ajuda");
+
+            //Sub-menus do menu "Ajuda"
+            ToolStripMenuItem sobreMenuItem = new ToolStripMenuItem("Sobre");
+            sobreMenuItem.Click += new EventHandler(SobreMenuItem_Click);
+            ajudaMenuItem.DropDownItems.Add(sobreMenuItem);
+
+            menuStrip.Items.Add(ajudaMenuItem);
+
+            this.Controls.Add(menuStrip);
+
+            //Configurações da barra de status
+            StatusStrip statusStrip = new StatusStrip();
+            statusStrip.Dock = DockStyle.Bottom;
+
+            ToolStripStatusLabel infoLabel = new ToolStripStatusLabel();
+            infoLabel.Text = ".gitignore .jussan";
+            statusStrip.Items.Add(infoLabel);
+
+            dateTimeLabel = new ToolStripStatusLabel(); //Cria a instância para a data e hora
+            statusStrip.Items.Add(dateTimeLabel); //Adiciona o label à barra de status
+
+            this.Controls.Add(statusStrip);
+        }
+
+        static class Program
+        {
+            [STAThread]
+            static void Main()
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new ProgramForm());
+            }
+        }
+    }
 }
--- View/Formulario/bombaForm/ListBomba.cs
+++ View/Formulario/bombaForm/ListBomba.cs
@@ -4,3 +4,167 @@
  * Data: 22/05/2023
  * Versão: 1.0
  */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ViewListBomba
+{
+    public partial class ListBombaForm : Form
+    {
+        private Button novoButton;
+        private Button alterarButton;
+        private Button excluirButton;
+        private Button cancelarButton;
+        private Button sairButton;
+        private DataGridView produtosDataGridView;
+
+        public ListBombaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            // Configurações da janela do formulário
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Text = "Listar Bombas";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+
+            // Configurações do botão de novo 
+            novoButton = new Button();
+            novoButton.Text = "Novo";
+            novoButton.Location = new Point(20, 340);
+            novoButton.Size = new Size(80, 30);
+            novoButton.Click += new EventHandler(NovoButton_Click);
+            this.Controls.Add(novoButton);
+
+            // Configurações do botão de alterar 
+            alterarButton = new Button();
+            alterarButton.Text = "Alterar";
+            alterarButton.Location = new Point(110, 340);
+            alterarButton.Size = new Size(80, 30);
+            alterarButton.Click += new EventHandler(AlterarButton_Click);
+            this.Controls.Add(alterarButton);
+
+            //Configurações do botão de excluir
+            excluirButton = new Button();
+            excluirButton.Text = "Excluir";
+            excluirButton.Location = new Point(200, 340);
+            excluirButton.Size = new Size(80, 30);
+            excluirButton.Click += new EventHandler(ExcluirButton_Click);
+            this.Controls.Add(excluirButton);
+
+            //Configurações do botão de cancelar
+            cancelarButton = new Button();
+            cancelarButton.Text = "Cancelar";
+            cancelarButton.Location = new Point(290, 340);
+            cancelarButton.Size = new Size(80, 30);
+            cancelarButton.Click += new EventHandler(CancelarButton_Click);
+            this.Controls.Add(cancelarButton);
+
+            //Configurações do botão de sair
+            sairButton = new Button();
+            sairButton.Text = "Sair";
+            sairButton.Location = new Point(380, 340);
+            sairButton.Size = new Size(80, 30);
+            sairButton.Click += new EventHandler(SairButton_Click);
+            this.Controls.Add(sairButton);
+
+            //Configurações do grid de Bombas
+            produtosDataGridView = new DataGridView();
+            produtosDataGridView.Location = new Point(20, 40);
+            produtosDataGridView.Size = new Size(440, 280);
+            produtosDataGridView.AllowUserToAddRows = false;
+            produtosDataGridView.AllowUserToDeleteRows = false;
+            produtosDataGridView.ReadOnly = true;
+            produtosDataGridView.MultiSelect = false;
+            produtosDataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            produtosDataGridView.AutoGenerateColumns = false;
+
+            //Configuração das colunas do grid
+            DataGridViewTextBoxColumn idColumn = new DataGridViewTextBoxColumn();
+            idColumn.DataPropertyName = "Bomba";//Lembrar que a exibição é nome + 
+            idColumn.HeaderText = "Bomba";
+            idColumn.Width = 50;
+            idColumn.ReadOnly = true;
+            produtosDataGridView.Columns.Add(idColumn);
+
+
+            DataGridViewTextBoxColumn precoColumn = new DataGridViewTextBoxColumn();
+            precoColumn.DataPropertyName = "Nome Combustível"; //JUSSAN - Lembrar de preparar a controller para buscar o nome do combustível no tipo combustível
+            precoColumn.HeaderText = "Nome Combustível";
+            precoColumn.Width = 70;
+            precoColumn.ReadOnly = true;
+            produtosDataGridView.Columns.Add(precoColumn);
+
+            DataGridViewTextBoxColumn quantidadeColumn = new DataGridViewTextBoxColumn();
+            quantidadeColumn.DataPropertyName = "Quantidade";
+            quantidadeColumn.HeaderText = "Quantidade";
+            quantidadeColumn.Width = 80;
+            quantidadeColumn.ReadOnly = true;
+            produtosDataGridView.Columns.Add(quantidadeColumn);
+
+            DataGridViewTextBoxColumn nomeColumn = new DataGridViewTextBoxColumn();
+            nomeColumn.DataPropertyName = "Nome";
+            nomeColumn.HeaderText = "Nome";
+            nomeColumn.Width = 150;
+            nomeColumn.ReadOnly = true;
+            produtosDataGridView.Columns.Add(nomeColumn);
+/*
+         BOMBA ID
+         TIPO_COMBUS_ID 
+         TIPO_COMBUS_NOME 
+         CAPACIDADE MAX 
+         CAPACIDADE MIN
+         MOV. ID
+         Movimentacao Movimentacao 
+*/
+
+
+
+            this.Controls.Add(produtosDataGridView);
+        }
+
+        private void NovoButton_Click(object sender, EventArgs e)
+        {
+            FrmNovoAlx alx = new FrmNovoAlx();
+            alx.Show();
+            //throw new NotImplementedException();
+        }
+
+        private void AlterarButton_Click(object sender, EventArgs e)
+        {
+            //Adicionar Ação/código para alterar uma bomba selecionada
+        }
+
+        private void ExcluirButton_Click(object sender, EventArgs e)
+        {
+            //Adicionar Ação/código para excluir uma bomba selecionada
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            //Adicionar Ação/código para cancelar a operação atual
+        }
+
+        private void SairButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Você realmente deseja sair?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+                //Application.Exit();
+            }
+        }
+    }
+}
